0570f105dd8962d518ad20914ca2eeb69006e58c
LucaGior <luca_gior@hotmail.com>
2024-11-15 16:29:48 -0300

    config app url

--- config/app.php
+++ config/app.php
@@ -1,6 +1,6 @@
 <?php
     /* Configuraciones de la pagina */
-    const APP_URL="http://localhost/GiordanoLucaProyectoAsistencia-main/proyecto_final_programacion/";
+    const APP_URL="http://localhost/ProyectoAsistencia/";
     const APP_NAME="asistencia programacion";
     const APP_SESSION_NAME="Asistencia";
     date_default_timezone_set("America/Argentina/Buenos_Aires");
